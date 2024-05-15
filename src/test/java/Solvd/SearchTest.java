44139785766369b66a2cb9babb47a7657dbf9f97
Ivan Castellanos <ivancastellanossaba@gmail.com>
2024-05-14 22:43:46 -0300

    feat: Added Assertions to the test searchProductTest2

--- src/test/java/Solvd/SearchTest.java
+++ src/test/java/Solvd/SearchTest.java
@@ -9,7 +9,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SearchTest extends BaseTest {
-    protected static Logger logger = LogManager.getLogger(BasePage.class);
+    protected static Logger logger = LogManager.getLogger(SearchTest.class);
 
     @Test
     public void searchProductTest() {
--- src/test/java/Solvd/SearchTestThread2.java
+++ src/test/java/Solvd/SearchTestThread2.java
@@ -1,16 +1,26 @@
 package Solvd;
 
+import Solvd.automationteststore.BasePage;
 import Solvd.automationteststore.HomePage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SearchTestThread2 extends BaseTest{
+    protected static Logger logger = LogManager.getLogger(SearchTestThread2.class);
 
-    @Test(enabled = false)
+
+    @Test()
     public void searchProductTest2() {
         WebDriver driver = getDriver();
         HomePage homePage = new HomePage(driver);
         homePage.open();
+        logger.info(" automation exercise page is opened");
+        Assert.assertTrue(homePage.isPageOpen(), "Page is not opened correctly in the HomePage.");
+        String logoText = homePage.getLogoText();
+        Assert.assertEquals(logoText, "Automation Test Store", "Logo text is not the same");
         homePage.searchForProduct("eyes");
         homePage.clickSearchButton();
         homePage.printTitles();
