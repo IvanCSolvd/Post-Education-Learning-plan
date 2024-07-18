7341159f30df178ed275dbc93d4bf03e50e4595b
Ivan Castellanos <ivancastellanossaba@gmail.com>
2024-07-17 23:47:51 -0300

    feat: added multiple browser execution that could be parametrized from the testng.xml

--- src/test/java/solvd/BaseTest.java
+++ src/test/java/solvd/BaseTest.java
@@ -5,13 +5,17 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BaseTest implements ITestListener {
     private static ThreadLocal<WebDriver> driverThreadLocal = new ThreadLocal<>();
@@ -22,9 +26,25 @@ public class BaseTest implements ITestListener {
 
 
     @BeforeMethod
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+    @Parameters("browser")
+    public void setUp(String browser) throws MalformedURLException {
+        WebDriver driver;
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                capabilities.setBrowserName("chrome");
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                capabilities.setBrowserName("firefox");
+                break;
+            default:
+                throw new IllegalArgumentException("Browser not supported");
+        }
+
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
         driver.manage().window().maximize();
         driverThreadLocal.set(driver);
     }
